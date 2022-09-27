a5345ebe2372d6546fdca871bdb48c0646b85654
Saymon Coppi <saymon@proxion.com.br>
2022-09-27 12:52:53 -0300

    remove cur_ vars, remove extra material check, update docstring

--- socket/socket_client.py
+++ socket/socket_client.py
@@ -1,4 +1,14 @@
+#! /usr/bin/env python
 """
+Program to extract data from PRIX TI400 Socket Server
+
+Run:
+socket_client.py -ip 192.168.0.x -p 9000
+
+Sample answer from TI400:
+Plataforma  0,497Ekg26/09/202215:04:55       04321
+
+... Check the manual to see all possible parameters
 """
 import socket
 import argparse
@@ -10,7 +20,7 @@ import os
 parser = argparse.ArgumentParser(description="Connect to PRIX TI400 Socket Server")
 parser.add_argument("-ip", "--ip_address", required=True)
 parser.add_argument("-p", "--port", required=True)
-parser.add_argument("-l", "--log", choices=["yes", "no"], default="yes", required=True)
+parser.add_argument("-l", "--log", choices=["yes", "no"], default="yes")
 options = parser.parse_args()
 
 tcp_ip = str(options.ip_address)
@@ -71,7 +81,6 @@ if is_connected:
         received_data = tcp.recv(BUFFER_SIZE).decode()
 
         if received_data:
-            # Sample data: Plataforma  0,497Ekg26/09/202215:04:55       04321
             if len(received_data) == 50:
                 ext_platform = received_data[:10].rstrip()
                 ext_material = received_data[-12:].lstrip()
@@ -79,31 +88,19 @@ if is_connected:
                 ext_weight_stable = received_data[17:18].lstrip()
                 ext_date_time_pre = received_data[20:38].lstrip()
                 ext_date_time = datetime.strptime(ext_date_time_pre, "%d/%m/%Y%H:%M:%S")
-                if ext_material:
-                    received_changes = f"{ext_material}|{ext_weight}"
-                else:
-                    print(
-                        f"Invalid Material! {ext_material} \nWaiting 5 seconds to try again...\n"
-                    )
-                    time.sleep(5)
+                received_changes = f"{ext_material}|{ext_weight}"
 
-                # if not check_changes == received_changes:
                 if (
                     (ext_weight_stable == "E")
                     and (not ext_weight == "0.000")
                     and (not check_changes == received_changes)
                 ):
                     check_changes = f"{ext_material}|{ext_weight}"
-                    cur_platform = ext_platform
-                    cur_material = ext_material
-                    cur_weight_stable = ext_weight_stable
-                    cur_weight = ext_weight
-                    cur_date_time = ext_date_time
 
-                    print(f"Platform:   {cur_platform}")
-                    print(f"Material:   {cur_material}")
-                    print(f"Weight:     {cur_weight}")
-                    print(f"Date/Time:  {cur_date_time}")
+                    print(f"Platform:   {ext_platform}")
+                    print(f"Material:   {ext_material}")
+                    print(f"Weight:     {ext_weight}")
+                    print(f"Date/Time:  {ext_date_time}")
                     print(
                         "----------------------------------------------------------------------"
                     )
